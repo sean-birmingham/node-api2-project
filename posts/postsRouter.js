f8fc098f108b3392bfe61b36422b08a4bc51c5c8
Sean Birmingham <seannbirmingham98@gmail.com>
2020-09-30 12:47:31 -0400

    able to get comments and make a post

--- posts/postsRouter.js
+++ posts/postsRouter.js
@@ -10,6 +10,7 @@ router.get('/', (req, res) => {
       res.status(200).json(posts);
     })
     .catch((err) => {
+      console.log(err);
       res
         .status(500)
         .json({ error: 'The posts information could not be retrieved' });
@@ -27,6 +28,49 @@ router.get('/:id', (req, res) => {
         .json({ message: 'The post with the specified ID does not exist' });
     }
   });
+});
+
+router.get('/:id/comments', (req, res) => {
+  const { id } = req.params;
+
+  db.findPostComments(id)
+    .then((comments) => {
+      if (comments) {
+        res.status(200).json(comments);
+      } else {
+        res
+          .status(404)
+          .json({ message: 'The post with the specified ID does not exist' });
+      }
+    })
+    .catch((err) => {
+      console.log(err);
+      res
+        .status(500)
+        .json({ error: 'The comments information could not be retrieved' });
+    });
+});
+
+router.post('/', (req, res) => {
+  const data = req.body;
+
+  if (data.title && data.contents) {
+    db.insert(data)
+      .then((post) => {
+        res.status(201).json(post);
+      })
+      .catch((err) => {
+        console.log(err);
+        res.status(500).json({
+          errorMessage:
+            'There was an error while saving the post to the database'
+        });
+      });
+  } else {
+    res
+      .status(400)
+      .json({ errorMessage: 'Please provide title and contents for the post' });
+  }
 });
 
 module.exports = router;
