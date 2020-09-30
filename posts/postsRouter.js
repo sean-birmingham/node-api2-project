1eff4ae8b94a44f267bc2e88b1bea47bd64ce3a1
Sean Birmingham <seannbirmingham98@gmail.com>
2020-09-30 12:59:48 -0400

    able to update and delete posts

--- posts/postsRouter.js
+++ posts/postsRouter.js
@@ -73,4 +73,51 @@ router.post('/', (req, res) => {
   }
 });
 
+router.put('/:id', (req, res) => {
+  const { id } = req.params;
+  const data = req.body;
+
+  if (data.title && data.contents) {
+    db.update(id, data)
+      .then((post) => {
+        if (post) {
+          res.status(200).json(post);
+        } else {
+          res
+            .status(404)
+            .json({ message: 'The post with the specified ID does not exist' });
+        }
+      })
+      .catch((err) => {
+        console.log(err);
+        res
+          .status(500)
+          .json({ error: 'The post information could not be modified' });
+      });
+  } else {
+    res
+      .status(400)
+      .json({ errorMessage: 'Please provide title and contents for the post' });
+  }
+});
+
+router.delete('/:id', (req, res) => {
+  const { id } = req.params;
+
+  db.remove(id)
+    .then((count) => {
+      if (count > 0) {
+        res.status(200).json({ message: 'The post has been deleted' });
+      } else {
+        res
+          .status(404)
+          .json({ message: 'The post with the specified ID does not exist' });
+      }
+    })
+    .catch((err) => {
+      console.log(err);
+      res.status(500).json({ error: 'The post could not be removed' });
+    });
+});
+
 module.exports = router;
